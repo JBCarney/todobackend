caad72b2491e82229a64203e27fa048e90b85f61
Jason Carney <jason@jbcarney.com>
2018-02-12 23:09:23 -0500

    Switched to me.

--- src/todobackend/settings/release.py
+++ src/todobackend/settings/release.py
@@ -15,8 +15,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
-        'USER': os.environ.get('MYSQL_USER','todo'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
+        'USER': os.environ.get('MYSQL_USER','root'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD','Helmsman@1864'),
         'HOST': os.environ.get('MYSQL_HOST','localhost'),
         'PORT': os.environ.get('MYSQL_PORT','3306'),
     }
